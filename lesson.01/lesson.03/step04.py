8948a15d478e1cb7e5fe5c7f5c14f2458b912e6d
Stanislav <42679599+Stanislavzzz@users.noreply.github.com>
2026-08-21 08:09:02 +0300

    Delete lesson.01/lesson.03 directory

--- lesson.01/lesson.03/step04.py
+++
@@ -1,7 +0,0 @@
-num_int = 17
-num_float = 3.14
-flag = True
-
-print(str(num_int))
-print(str(num_float))
-print(str(flag))
--- lesson.01/lesson.03/step09.py
+++
@@ -1,9 +0,0 @@
-text_hello = "Привет, мИр!"
-
-print(text_hello.upper())
-print(text_hello.lower())
-print(text_hello.capitalize())
-print(text_hello.title())
-print(text_hello.swapcase())
-
-# print(text_hello)
